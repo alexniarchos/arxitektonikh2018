224e29f7d9e5dfeaeecc1a505342f985685d39b4
Your Name <you@example.com>
2018-12-15 20:12:19 +0200

    KENKEN

--- project1/project1.s
+++ project1/project1.s
@@ -1,10 +1,10 @@
 .data
     array:  
-        .word 0x5410402200200000
+        .word 0x43C0000000000000
 
     len:  .word 150
     
-    mask: .word 0x7FF0000000000000,0x000FFFFFFFFFFFFF,0x0000000000000001,0x0010000000000000
+    mask: .word 0x7FF0000000000000,0x000FFFFFFFFFFFFF,0x0000000000000001,0x0010000000000000,0x7FFFFFFFFFFFFFFF,0x8000000000000000
 
     CONTROL: .word 0x10000
 	DATA:    .word 0x10008
@@ -29,9 +29,8 @@ main:
     and r4,r6,r5            # r4 sign
     daddi r10,$zero,24
     ld r6,mask(r10)
-    halt
     slt r11,r2,$zero
-    beq r11,$zero,negative_exp # check exponent 8etikos h arnhtikos
+    bne r11,$zero,negative_exp # check exponent 8etikos h arnhtikos
     # 8etikos h oudeteros
     slti r11,r2,62 # overflow check
     beq r11,$zero,overflow # overflow check
@@ -47,15 +46,41 @@ loop:
     daddi r8,$zero,1 # afairesh apo exponent
     dsub r2,r2,r8 # afairesh apo exponent
     jal loop
-endloop: # telos loupas
-        # teleutaio shift mantisa
-        # an to bit pou bgei aristera twn 52 einai 1
-        # tote stroggulopoihsh pros ta epanw
-        # alliws stroggulopoihsh pros ta katw
-        # ftiaxnw ton tupo ths mantissas
+endloop: 
+    dsll r3,r3,1    # teleutaio shift mantisa
+    and r7,r3,r6    # an to bit pou bgei aristera twn 52 einai 1
+    daddi r8,$zero,52  # ka8e stoixeio p baginei aristera twn 52 xamhlwn bit to pros8etw ston deutero kataxwrhth: bazw sto kataxwrhth pou 8a xrhsimopoihsw san noumero to poses fores na kanei shift
+    dsrlv r7,r7,r8
+    beq r7,$zero,roundDown
+    ld r9,array(r10)
+    beq r5,r9,roundDown
+    daddi r10,$zero,32
+    daddi r5,r5,1   # tote stroggulopoihsh pros ta epanw
+    roundDown:      # alliws stroggulopoihsh pros ta katw
+    beq r4,$zero,finish
+    daddi r8,$zero,-1
+    dmul r5,r5,r8
+finish:
+    halt
 overflow:
-
+    beq r4,$zero,posoverflow
+    daddi r10,$zero,40
+    ld r5,mask(r10)
+    halt
+posoverflow:
+    daddi r10,$zero,32
+    ld r5,mask(r10)
+    halt
 negative_exp:  # arnhtiko exponent
-    daddi r13,$zero,2
-        # tsekarw ton exponent an einai >2 apeu8eias 0 an einai 1 tote 1 h -1 analoga to sign
+    daddi r12,$zero,-2
+    slt r10,r12,r2 # tsekarw ton exponent an einai <-1 apeu8eias 0 an einai -1 tote 1 h -1 analoga to sign
+    bne r10,$zero,applysign
+    dadd r5,$zero,$zero                   
+    halt
+applysign:
+    beq r4,$zero,pos
+    daddi r5,$zero,-1
+    halt
+pos:
+    daddi r5,$zero,1
     halt
