0aef45061e3637822b3240eb806b5c7c6e1cc40f
Your Name <you@example.com>
2018-12-16 19:05:49 +0200

    DONE

--- project1/project1.s
+++ project1/project1.s
@@ -1,10 +1,102 @@
 .data
-    array:  
-        .word 0x7FF0000000000000,0x7FF0000000000000
+    A:  
+        .word 0xdc468b8c847dd334,0x9e01794de636dc67,0x0b0cb1960d169db8,0x470bc36e1ec57160,0x8e7ba728419e10ef,0x485ffd8b093645e7
+        .word 0xa5ce31a776ac45bd,0x1e5bda477953eb1c,0xc44c1f6397823de6,0x164e578ebf5dc9f3,0x7bfcb33e0203a78f,0x44a138992d451a17
+        .word 0x3930ac7b546451bd,0xeec5f3253df19892,0x19ce1a71c3c48076,0x7975d88993e78602,0x9c014ab9f1a5c114,0x8125966d72542168
+        .word 0x9652bbbc2c651e82,0xc393aa08e92c9654,0x818b860afc4ea077,0xf98960c14edbc424,0xc4cc6ba8eb8d4f96,0x15d63669d2afe300
+        .word 0xe27e4d07433e85e2,0x432f021626a008d2,0xa1e92cc4028d6551,0xdf45cb9d3ded6286,0xfbe2a48e124d74c3,0x97ec73c80100d794
+        .word 0xfb5b6e8d63462613,0x0b35140fc8e0cb45,0xf453161858f804e5,0x3b1d66d0b2f4f6ee,0x81749cb461708e67,0x588ada0aad3a3db7
+        .word 0xf660dac7969ee12c,0xfa3aa63091082f8f,0x5ba04524d5868ddb,0xbec72b0430b61034,0xb147ca35654307da,0x09e4a225be356670
+        .word 0x04fc3892ee64b3c4,0xa1838d8140b6e0cb,0xd532ed9a043061f4,0xe8a884d27d7b78f3,0x9ee772be29b2e638,0x77e8f5ffe8c0f0f7
+        .word 0xc18bff43a366325b,0xa21d893df4df482b,0xfff06038da7bd382,0xf1fb0fbc05f46b89,0xccc4c67701fb7e88,0xe74975356659a5f5
+        .word 0x8ecaaeaaa8ec98b3,0x58373bcdb5b4bbb9,0xffec828737a0b0b0,0x0fde110fce9d0512,0x881667b47982d919,0x173e63eae3a55dec
+        .word 0x6919bdfcac1b0f51,0x1616329391ca1ebd,0x36a9d309a4107497,0xd196ea795cb978cd,0xe9dba1a8a8cd9971,0x93c7fa462a025176
+        .word 0x0a5dc138d564ab03,0xe111bc85f5b3feac,0xca8c5c16b739d24d,0xa4d7ffeff2163b9a,0xc974fe7e50a083cc,0x03a273071ef2b3f4
+        .word 0x19dedb353cdaf474,0xfa3aa795785223a6,0xa0371ccc7900b8af,0x668142467f6c2c5d,0x623ae7735dbebfb4,0x2c7d7df56d4e9f4f
+        .word 0x1e26cc6da58806f3,0xca98c8b4c1bb5c8b,0xc86376ae03d27825,0x58a1b96e6abe3eaa,0x17aea0305a686e89,0x73ab2e4d81a4f112
+        .word 0x10163ad2e4b82773,0x975de1ae8466fa48,0x324981d577e4fe05,0x6792c5d9fc46f16f,0xe209fd35033c972c,0x18ad553d2cdfa40c
+        .word 0x185bbc4213cb690b,0x0c47141d952f6d22,0xd726f67ae634fc6c,0xa24db84ee509c338,0x1ba5d4cead1dd1a2,0xba9145a07d073b4d
+        .word 0xaa72473eca41e88a,0x6924d5daf52bc5da,0x5643e1f8924e66fb,0xfb93d2a0f3c2ecac,0xc5626334522ae52a,0xeca8d452f7ed3eb3
+        .word 0xe19814093797b50a,0x1223244b67f6ba1e,0x30f1affe6394d0f8,0x36c4f8699d11c650,0x1cf089bc74590a24,0x4e408db500902e4e
+        .word 0x2201ede8acbd15de,0xea3a6bd812ff0bf3,0x0648b12f9ec18908,0x34f6337b7357ee8e,0x657eb3ca408960f1,0xa5cbfce3a18ab2f2
+        .word 0x9d8db987c21971f5,0x0cded9f623a9760b,0x125eefc55a72c7e0,0x2ca114993dddf904,0xc9ca499e35aae31c,0x44c35dfb12c62330
+        .word 0x0715f158e859007b,0xd7dbb72e89863d20,0x8817b00f649b320c,0xe750f41384e43cdb,0xdcd812a019a5982b,0xdb6ee4c291ebe506
+        .word 0xda3c45d95754133e,0x3f7e727f024c5ffc,0x7333eb858fc3fad6,0x6b8e0a100316d258,0xa9df88f9dbebadae,0xf5eeee62a3cf8b6d
+        .word 0x52d784d99628b245,0xb091404ae037e9ba,0x41be8ea9a78ef1d5,0xea63c19226aea461,0x7e8890125e38fe79,0x3278f328705c3ce8
+        .word 0x770a483939c5727e,0xcb4170c41049716e,0x83f4c0789ddbdbba,0x94250e4aa486c7dd,0x2c3531f89301f254,0x75c342e852eef22b
+        .word 0x4e7c73b87a42c714,0xaa3e17c816adae86,0x3c0ad04b835dc5af,0xbdc811ee3328e9b2,0xa252e99c4b027cb4,0x4913b35f141a5b6a
 
-    len:  .word 2
+    B:  
+        .word 0xdc468b8c847dd334,0x9e01794de636dc67,0x0b0cb1960d169db8,0x470bc36e1ec57160,0x8e7ba728419e10ef,0x485ffd8b093645e7
+        .word 0xa5ce31a776ac45bd,0x1e5bda477953eb1c,0xc44c1f6397823de6,0x164e578ebf5dc9f3,0x7bfcb33e0203a78f,0x44a138992d451a17
+        .word 0x3930ac7b546451bd,0xeec5f3253df19892,0x19ce1a71c3c48076,0x7975d88993e78602,0x9c014ab9f1a5c114,0x8125966d72542168
+        .word 0x9652bbbc2c651e82,0xc393aa08e92c9654,0x818b860afc4ea077,0xf98960c14edbc424,0xc4cc6ba8eb8d4f96,0x15d63669d2afe300
+        .word 0xe27e4d07433e85e2,0x432f021626a008d2,0xa1e92cc4028d6551,0xdf45cb9d3ded6286,0xfbe2a48e124d74c3,0x97ec73c80100d794
+        .word 0xfb5b6e8d63462613,0x0b35140fc8e0cb45,0xf453161858f804e5,0x3b1d66d0b2f4f6ee,0x81749cb461708e67,0x588ada0aad3a3db7
+        .word 0xf660dac7969ee12c,0xfa3aa63091082f8f,0x5ba04524d5868ddb,0xbec72b0430b61034,0xb147ca35654307da,0x09e4a225be356670
+        .word 0x04fc3892ee64b3c4,0xa1838d8140b6e0cb,0xd532ed9a043061f4,0xe8a884d27d7b78f3,0x9ee772be29b2e638,0x77e8f5ffe8c0f0f7
+        .word 0xc18bff43a366325b,0xa21d893df4df482b,0xfff06038da7bd382,0xf1fb0fbc05f46b89,0xccc4c67701fb7e88,0xe74975356659a5f5
+        .word 0x8ecaaeaaa8ec98b3,0x58373bcdb5b4bbb9,0xffec828737a0b0b0,0x0fde110fce9d0512,0x881667b47982d919,0x173e63eae3a55dec
+        .word 0x6919bdfcac1b0f51,0x1616329391ca1ebd,0x36a9d309a4107497,0xd196ea795cb978cd,0xe9dba1a8a8cd9971,0x93c7fa462a025176
+        .word 0x0a5dc138d564ab03,0xe111bc85f5b3feac,0xca8c5c16b739d24d,0xa4d7ffeff2163b9a,0xc974fe7e50a083cc,0x03a273071ef2b3f4
+        .word 0x19dedb353cdaf474,0xfa3aa795785223a6,0xa0371ccc7900b8af,0x668142467f6c2c5d,0x623ae7735dbebfb4,0x2c7d7df56d4e9f4f
+        .word 0x1e26cc6da58806f3,0xca98c8b4c1bb5c8b,0xc86376ae03d27825,0x58a1b96e6abe3eaa,0x17aea0305a686e89,0x73ab2e4d81a4f112
+        .word 0x10163ad2e4b82773,0x975de1ae8466fa48,0x324981d577e4fe05,0x6792c5d9fc46f16f,0xe209fd35033c972c,0x18ad553d2cdfa40c
+        .word 0x185bbc4213cb690b,0x0c47141d952f6d22,0xd726f67ae634fc6c,0xa24db84ee509c338,0x1ba5d4cead1dd1a2,0xba9145a07d073b4d
+        .word 0xaa72473eca41e88a,0x6924d5daf52bc5da,0x5643e1f8924e66fb,0xfb93d2a0f3c2ecac,0xc5626334522ae52a,0xeca8d452f7ed3eb3
+        .word 0xe19814093797b50a,0x1223244b67f6ba1e,0x30f1affe6394d0f8,0x36c4f8699d11c650,0x1cf089bc74590a24,0x4e408db500902e4e
+        .word 0x2201ede8acbd15de,0xea3a6bd812ff0bf3,0x0648b12f9ec18908,0x34f6337b7357ee8e,0x657eb3ca408960f1,0xa5cbfce3a18ab2f2
+        .word 0x9d8db987c21971f5,0x0cded9f623a9760b,0x125eefc55a72c7e0,0x2ca114993dddf904,0xc9ca499e35aae31c,0x44c35dfb12c62330
+        .word 0x0715f158e859007b,0xd7dbb72e89863d20,0x8817b00f649b320c,0xe750f41384e43cdb,0xdcd812a019a5982b,0xdb6ee4c291ebe506
+        .word 0xda3c45d95754133e,0x3f7e727f024c5ffc,0x7333eb858fc3fad6,0x6b8e0a100316d258,0xa9df88f9dbebadae,0xf5eeee62a3cf8b6d
+        .word 0x52d784d99628b245,0xb091404ae037e9ba,0x41be8ea9a78ef1d5,0xea63c19226aea461,0x7e8890125e38fe79,0x3278f328705c3ce8
+        .word 0x770a483939c5727e,0xcb4170c41049716e,0x83f4c0789ddbdbba,0x94250e4aa486c7dd,0x2c3531f89301f254,0x75c342e852eef22b
+        .word 0x4e7c73b87a42c714,0xaa3e17c816adae86,0x3c0ad04b835dc5af,0xbdc811ee3328e9b2,0xa252e99c4b027cb4,0x4913b35f141a5b6a
+
+    C:  
+        .word 0xdc468b8c847dd334,0x9e01794de636dc67,0x0b0cb1960d169db8,0x470bc36e1ec57160,0x8e7ba728419e10ef,0x485ffd8b093645e7
+        .word 0xa5ce31a776ac45bd,0x1e5bda477953eb1c,0xc44c1f6397823de6,0x164e578ebf5dc9f3,0x7bfcb33e0203a78f,0x44a138992d451a17
+        .word 0x3930ac7b546451bd,0xeec5f3253df19892,0x19ce1a71c3c48076,0x7975d88993e78602,0x9c014ab9f1a5c114,0x8125966d72542168
+        .word 0x9652bbbc2c651e82,0xc393aa08e92c9654,0x818b860afc4ea077,0xf98960c14edbc424,0xc4cc6ba8eb8d4f96,0x15d63669d2afe300
+        .word 0xe27e4d07433e85e2,0x432f021626a008d2,0xa1e92cc4028d6551,0xdf45cb9d3ded6286,0xfbe2a48e124d74c3,0x97ec73c80100d794
+        .word 0xfb5b6e8d63462613,0x0b35140fc8e0cb45,0xf453161858f804e5,0x3b1d66d0b2f4f6ee,0x81749cb461708e67,0x588ada0aad3a3db7
+        .word 0xf660dac7969ee12c,0xfa3aa63091082f8f,0x5ba04524d5868ddb,0xbec72b0430b61034,0xb147ca35654307da,0x09e4a225be356670
+        .word 0x04fc3892ee64b3c4,0xa1838d8140b6e0cb,0xd532ed9a043061f4,0xe8a884d27d7b78f3,0x9ee772be29b2e638,0x77e8f5ffe8c0f0f7
+        .word 0xc18bff43a366325b,0xa21d893df4df482b,0xfff06038da7bd382,0xf1fb0fbc05f46b89,0xccc4c67701fb7e88,0xe74975356659a5f5
+        .word 0x8ecaaeaaa8ec98b3,0x58373bcdb5b4bbb9,0xffec828737a0b0b0,0x0fde110fce9d0512,0x881667b47982d919,0x173e63eae3a55dec
+        .word 0x6919bdfcac1b0f51,0x1616329391ca1ebd,0x36a9d309a4107497,0xd196ea795cb978cd,0xe9dba1a8a8cd9971,0x93c7fa462a025176
+        .word 0x0a5dc138d564ab03,0xe111bc85f5b3feac,0xca8c5c16b739d24d,0xa4d7ffeff2163b9a,0xc974fe7e50a083cc,0x03a273071ef2b3f4
+        .word 0x19dedb353cdaf474,0xfa3aa795785223a6,0xa0371ccc7900b8af,0x668142467f6c2c5d,0x623ae7735dbebfb4,0x2c7d7df56d4e9f4f
+        .word 0x1e26cc6da58806f3,0xca98c8b4c1bb5c8b,0xc86376ae03d27825,0x58a1b96e6abe3eaa,0x17aea0305a686e89,0x73ab2e4d81a4f112
+        .word 0x10163ad2e4b82773,0x975de1ae8466fa48,0x324981d577e4fe05,0x6792c5d9fc46f16f,0xe209fd35033c972c,0x18ad553d2cdfa40c
+        .word 0x185bbc4213cb690b,0x0c47141d952f6d22,0xd726f67ae634fc6c,0xa24db84ee509c338,0x1ba5d4cead1dd1a2,0xba9145a07d073b4d
+        .word 0xaa72473eca41e88a,0x6924d5daf52bc5da,0x5643e1f8924e66fb,0xfb93d2a0f3c2ecac,0xc5626334522ae52a,0xeca8d452f7ed3eb3
+        .word 0xe19814093797b50a,0x1223244b67f6ba1e,0x30f1affe6394d0f8,0x36c4f8699d11c650,0x1cf089bc74590a24,0x4e408db500902e4e
+        .word 0x2201ede8acbd15de,0xea3a6bd812ff0bf3,0x0648b12f9ec18908,0x34f6337b7357ee8e,0x657eb3ca408960f1,0xa5cbfce3a18ab2f2
+        .word 0x9d8db987c21971f5,0x0cded9f623a9760b,0x125eefc55a72c7e0,0x2ca114993dddf904,0xc9ca499e35aae31c,0x44c35dfb12c62330
+        .word 0x0715f158e859007b,0xd7dbb72e89863d20,0x8817b00f649b320c,0xe750f41384e43cdb,0xdcd812a019a5982b,0xdb6ee4c291ebe506
+        .word 0xda3c45d95754133e,0x3f7e727f024c5ffc,0x7333eb858fc3fad6,0x6b8e0a100316d258,0xa9df88f9dbebadae,0xf5eeee62a3cf8b6d
+        .word 0x52d784d99628b245,0xb091404ae037e9ba,0x41be8ea9a78ef1d5,0xea63c19226aea461,0x7e8890125e38fe79,0x3278f328705c3ce8
+        .word 0x770a483939c5727e,0xcb4170c41049716e,0x83f4c0789ddbdbba,0x94250e4aa486c7dd,0x2c3531f89301f254,0x75c342e852eef22b
+        .word 0x4e7c73b87a42c714,0xaa3e17c816adae86,0x3c0ad04b835dc5af,0xbdc811ee3328e9b2,0xa252e99c4b027cb4,0x4913b35f141a5b6a
+
+    len:  .word 1
     
     mask: .word 0x7FF0000000000000,0x000FFFFFFFFFFFFF,0x0000000000000001,0x0010000000000000,0x7FFFFFFFFFFFFFFF,0x8000000000000000
+
+    Pout:  .asciiz "P="
+    Nout:  .asciiz "N="
+    Dout:  .asciiz "D="
+    Tout:  .asciiz "T="
+    Zout:  .asciiz "Z="
+    Iout:  .asciiz "I="
+    P: .word 0x0000000000000000
+    N: .word 0x0000000000000000
+    D: .word 0x0000000000000000
+    T: .word 0x0000000000000000
+    Z: .word 0x0000000000000000
+    I: .word 0x0000000000000000
+
 
     CONTROL: .word 0x10000
 	DATA:    .word 0x10008
@@ -21,7 +113,7 @@ bigloop:
     slt r19,r23,r24
     beq r19,$zero,end
     daddi r20,$zero,0
-    ld r1,array(r23)
+    ld r1,A(r23)
     ld r3,mask($zero)
     and r2,r1,r3
     daddi r10,$zero,52
@@ -64,7 +156,7 @@ endloop:
     daddi r8,$zero,52  # ka8e stoixeio p baginei aristera twn 52 xamhlwn bit to pros8etw ston deutero kataxwrhth: bazw sto kataxwrhth pou 8a xrhsimopoihsw san noumero to poses fores na kanei shift
     dsrlv r7,r7,r8
     beq r7,$zero,roundDown
-    ld r9,array(r10)
+    ld r9,A(r10)
     beq r5,r9,roundDown
     daddi r10,$zero,32
     daddi r5,r5,1   # tote stroggulopoihsh pros ta epanw
@@ -124,21 +216,74 @@ checkabnormal:
     daddi r25,r25,1
     jal overflow
 cvt: 
-    l.d f2,array(r23)
-    daddi r23,r23,8
+    l.d f2,A(r23)
     cvt.l.d f2,f2
     mfc1 r6,f2
     bne r20,$zero,round
-    jal bigloop
+    jal store
 
 round:
     bne r4,$zero,neg
     daddi r6,r6,1
-    jal bigloop
+    jal store
 
 neg:
     daddi r6,r6,-1
+    jal store
+
+store:
+    sd r5,B(r23)
+    dsub r6,r6,r5
+    sd r6,C(r23)
+    daddi r23,r23,8
     jal bigloop
 
 end:
+    lwu r21,CONTROL(r0) # r25-I,  r27-N, r28-D, r29-T, r30-Z
+    lwu r22,DATA(r0)    # r21 a
+	daddi r20,r0,1;
+    daddi r23,r0,4;
+    #################
+    sd r26,P($zero)
+    sd r27,N($zero)
+    sd r28,D($zero)
+    sd r29,T($zero)
+    sd r30,Z($zero)
+    sd r25,I($zero)
+    ################# r26-P
+    daddi r24,r0,Pout;
+    sd r24,(r22);
+	sd r23,(r21);
+	sd r26,(r22);
+	sd r20,(r21);
+    ################# r27-N
+    daddi r24,r0,Nout;
+    sd r24,(r22);
+	sd r23,(r21);
+	sd r27,(r22);
+	sd r20,(r21);
+    ################# r28-D
+    daddi r24,r0,Dout;
+    sd r24,(r22);
+	sd r23,(r21);
+	sd r28,(r22);
+	sd r20,(r21);
+    ################# r29-T
+    daddi r24,r0,Tout;
+    sd r24,(r22);
+	sd r23,(r21);
+	sd r29,(r22);
+	sd r20,(r21);
+    ################# r30-Z
+    daddi r24,r0,Zout;
+    sd r24,(r22);
+	sd r23,(r21);
+	sd r30,(r22);
+	sd r20,(r21);
+    ################# r25-I
+    daddi r24,r0,Iout;
+    sd r24,(r22);
+	sd r23,(r21);
+	sd r25,(r22);
+	sd r20,(r21);
     halt
